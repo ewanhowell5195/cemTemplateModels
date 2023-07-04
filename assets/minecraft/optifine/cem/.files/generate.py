67ee0e6c4e48d1227696dc5f0d9fcec21894551b
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2023-07-04 21:52:20 +0100

    subdir support

--- assets/minecraft/optifine/cem/.files/generate.py
+++ assets/minecraft/optifine/cem/.files/generate.py
@@ -1,4 +1,4 @@
-from os import walk, path, getcwd
+from os import walk, path, getcwd, listdir
 from json import load, dumps, loads
 from base64 import b64encode
 
@@ -11,39 +11,57 @@ output["categories"] = [{
 	"entities": []
 }]
 
+models = []
+
 for subdir, dirs, files in walk("../"):
 	for file in files:
 		if file.endswith(".jem"):
 			filepath = path.join(subdir, file)
 			entityName = path.basename(filepath)[:-4]
-			with open(filepath, "r") as jsonfile:
-				data = load(jsonfile)
-			data.pop("texture", None)
-			for item in list(data):
-				if item not in ["texture", "textureSize", "shadowSize", "models", "model"]:
-					data.pop(item)
-				elif item == "models":
-					x = 0
-					for dictionary in data[item]:
-						if "boxes" in list(dictionary) or "submodels" in list(dictionary) or "model" in list(dictionary):
-							pass
-						else:
-							for item2 in list(dictionary):
-								if not item2 == "part":
-									data[item][x].pop(item2, None)
-						x += 1
-			with open(f"{filepath[:-4]}.png", "rb") as textureFile:
-				encodedImage = b64encode(textureFile.read())
-			output["models"][entityName] = {
-				"texture_data": encodedImage.decode(),
-				"model": dumps(data, separators = (',', ':'))
-			}
-			output["categories"][0]["entities"].append({
-					"name": entityName,
-					"file_name": entityName,
-					"display_name": entityName.replace("_", " ").title(),
-					"texture_name": entityName
-				})
+			models.append([filepath, entityName])
+	if subdir == "../":
+		for directory in dirs:
+			if directory == ".files":
+				continue
+			for subdir, dirs, files in walk(path.join("../", directory)):
+				for file in files:
+					if file.endswith(".jem"):
+						filepath = path.join(subdir, file)
+						entityName = path.basename(filepath)[:-4]
+						print(filepath)
+						models.append([filepath, entityName])
+
+models = sorted(models, key=lambda x: x[1])
+
+for [filepath, entityName] in models:
+	with open(filepath, "r") as jsonfile:
+		data = load(jsonfile)
+	data.pop("texture", None)
+	for item in list(data):
+		if item not in ["texture", "textureSize", "shadowSize", "models", "model"]:
+			data.pop(item)
+		elif item == "models":
+			x = 0
+			for dictionary in data[item]:
+				if "boxes" in list(dictionary) or "submodels" in list(dictionary) or "model" in list(dictionary):
+					pass
+				else:
+					for item2 in list(dictionary):
+						if not item2 == "part":
+							data[item][x].pop(item2, None)
+				x += 1
+	with open(f"{filepath[:-4]}.png", "rb") as textureFile:
+		encodedImage = b64encode(textureFile.read())
+	output["models"][entityName] = {
+		"texture_data": encodedImage.decode(),
+		"model": dumps(data, separators = (',', ':'))
+	}
+	output["categories"][0]["entities"].append({
+		"name": entityName,
+		"file_name": entityName,
+		"display_name": entityName.replace("_", " ").title(),
+		"texture_name": entityName
+	})
 
 with open("models.json", "w") as outFile:
 	outFile.write(dumps(output, indent = "\t"))
